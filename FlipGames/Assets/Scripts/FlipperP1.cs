0b73b8e341b748c89ed0c6aa68d96090ba642894
Quinten Reijns <529357@student.glu.nl>
2020-02-04 14:34:20 +0100

    ..

--- FlipGames/Assets/Scripts/FlipperP1.cs
+++ FlipGames/Assets/Scripts/FlipperP1.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class FlipperP1 : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject Flipper1;
+
+   
     void Start()
     {
         
     }
 
-    // Update is called once per frame
     void Update()
     {
         
