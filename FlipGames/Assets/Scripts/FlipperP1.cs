34d5afbcfa6f42b2c04b522de5ed8f4079c585ac
Quinten Reijns <529357@student.glu.nl>
2020-02-05 15:26:20 +0100

    Prototype

--- FlipGames/Assets/Scripts/FlipperP1.cs
+++ FlipGames/Assets/Scripts/FlipperP1.cs
@@ -8,14 +8,31 @@ public class FlipperP1 : MonoBehaviour
     public float pressedPostion = 45f;
     public float hitStrenght = 10000f;
     public float flipperDamper = 150f;
+    HingeJoint hinge;
 
     public string inputName;
 
+    private void Start()
+    {
+        hinge = GetComponent<HingeJoint>();
+        hinge.useSpring = true;
+    }
+
     private void Update()
     {
+        JointSpring spring = new JointSpring();
+        spring.spring = hitStrenght;
+        spring.damper = flipperDamper;
+
         if (Input.GetAxis(inputName) == 1)
         {
-
+            spring.targetPosition = pressedPostion;
         }
+        else
+        {
+            spring.targetPosition = resPosition;
+        }
+        hinge.spring = spring;
+        hinge.useLimits = true;
     }
 }
