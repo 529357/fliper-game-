295d2b4ae38e195425d35bd9056a5327159c9e22
Quinten Reijns <529357@student.glu.nl>
2020-02-05 12:21:46 +0100

    Spawning

--- FlipGames/Assets/Scripts/KnikkersSpawn.cs
+++ FlipGames/Assets/Scripts/KnikkersSpawn.cs
@@ -1,18 +1,52 @@
-﻿using System.Collections;
+﻿
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class KnikkersSpawn : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public GameObject junkFood1, junkFood2, junkFood3, junkFood4, healthyFood1, healthyFood2, healthyFood3, healthyFood4;
 
-    // Update is called once per frame
+    public float spawnRate = 2f;
+
+    float nextSpawn = 0f;
+
+    int whatToSpawn;
+
     void Update()
     {
-        
+        if (Time.time > nextSpawn)
+        {
+            whatToSpawn = Random.Range(1, 6);
+            Debug.Log(whatToSpawn);
+
+            switch (whatToSpawn) {
+                case 1:
+                    Instantiate(junkFood1, transform.position, Quaternion.identity);
+                    break;
+                case 2:
+                    Instantiate(junkFood2, transform.position, Quaternion.identity);
+                    break;
+                case 3:
+                    Instantiate(junkFood3, transform.position, Quaternion.identity);
+                    break;
+                case 4:
+                    Instantiate(junkFood4, transform.position, Quaternion.identity);
+                    break;
+                case 5:
+                    Instantiate(healthyFood1, transform.position, Quaternion.identity);
+                    break;
+                case 6:
+                    Instantiate(healthyFood2, transform.position, Quaternion.identity);
+                    break;
+                case 7:
+                    Instantiate(healthyFood3, transform.position, Quaternion.identity);
+                    break;
+                case 8:
+                    Instantiate(healthyFood4, transform.position, Quaternion.identity);
+                    break;
+            }
+            nextSpawn = Time.time + spawnRate;
+        }     
     }
 }
