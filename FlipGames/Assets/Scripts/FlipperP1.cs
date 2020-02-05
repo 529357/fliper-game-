ab9be8c5dc6ae0cd3bf96c5cf175a8b00c34ecb4
Quinten Reijns <529357@student.glu.nl>
2020-02-05 14:36:23 +0100

    Flippers

--- FlipGames/Assets/Scripts/FlipperP1.cs
+++ FlipGames/Assets/Scripts/FlipperP1.cs
@@ -4,16 +4,18 @@ using UnityEngine;
 
 public class FlipperP1 : MonoBehaviour
 {
-    public GameObject Flipper1;
+    public float resPosition = 0f;
+    public float pressedPostion = 45f;
+    public float hitStrenght = 10000f;
+    public float flipperDamper = 150f;
 
-   
-    void Start()
-    {
-        
-    }
+    public string inputName;
 
-    void Update()
+    private void Update()
     {
-        
+        if (Input.GetAxis(inputName) == 1)
+        {
+
+        }
     }
 }
